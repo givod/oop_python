7e346245160ea888ee561ba863d3cb494b3f9b6b
Moses Nero Vodounou <mvodounou@outlook.com>
2021-11-23 00:29:39 +0100

    done

---
+++ multiple-inheritance.py
@@ -0,0 +1,19 @@
+class A:
+    def __init__(self):
+        super().__init__()
+        self.foo = "foo"
+
+class B:
+    def __init__(self):
+        super().__init__()
+        self.bar = "bar"
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
+
+    def showprops(self):
+        print(self.foo)
+        print(self.bar)
+
+c = C()
